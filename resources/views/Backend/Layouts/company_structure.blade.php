ea8e1c38bd089c63fa8c644ac1c6634606e3ad75
Tajim Islam <tjm4181@gmail.com>
2023-11-22 17:43:45 +0600

    company_structure_updated

--- resources/views/Backend/Layouts/company_structure.blade.php
+++ resources/views/Backend/Layouts/company_structure.blade.php
@@ -21,6 +21,11 @@ Use App\Models\admin_branch_info;
 Use App\Models\branch_info;
 
 @endphp
+<style>
+    div#ticket {
+    padding: 0px;
+}
+    </style>
 
 <!-- [ Main Content ] start -->
 <div class="pcoded-main-container">
@@ -31,6 +36,7 @@ Use App\Models\branch_info;
                 <div class="row">
                     <div class="col-sm-3 col-lg-3 mb-3 link-box">
                         <p><b>Welcome back, SUPER ADMIN !</b></p>
+                        
                         <span class="fs-semibold text-muted">Track your somity activity, leads and deals here.</span>
                     </div>
                     <div class="col-sm-3 col-lg-3 mb-3 link-box">
@@ -435,6 +441,231 @@ Use App\Models\branch_info;
                                     <h5>{{$totals['total_deposit_provide']}} /-</h5>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xl-3">
+                <div class="card flat-card">
+                    <div class="row-table">
+                        <div class="col-md-4 col-sm-2 card-body">
+                             <img src="{{asset('Backend/images/icon')}}/trend.png" alt="">     
+                        </div>
+                        <div class="col-sm-8 card-body" id="ticket">
+                        <div class="text-center">
+                            <b>আজকের ঋণ আদায়যোগ্য</b><br>
+                            <h5>00/-</h5>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xl-3">
+                <div class="card flat-card">
+                    <div class="row-table">
+                        <div class="col-md-4 col-sm-2 card-body">
+                             <img src="{{asset('Backend/images/icon')}}/attachment.png" alt="">     
+                        </div>
+                        <div class="col-sm-8 card-body" id="ticket">
+                        <div class="text-center">
+                            <b>আজকের ঋণ আদায়</b><br>
+                            <h5>00/-</h5>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xl-3">
+                <div class="card flat-card">
+                    <div class="row-table">
+                        <div class="col-md-4 col-sm-2 card-body">
+                             <img src="{{asset('Backend/images/icon')}}/coins.png" alt="">     
+                        </div>
+                        <div class="col-sm-8 card-body" id="ticket">
+                        <div class="text-center">
+                            <b>আজকের ঋণ বকেয়া</b><br>
+                            <h5>00/-</h5>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xl-3">
+                <div class="card flat-card">
+                    <div class="row-table">
+                        <div class="col-md-4 col-sm-2 card-body">
+                             <img src="{{asset('Backend/images/icon')}}/invoice.png" alt="">     
+                        </div>
+                        <div class="col-sm-8 card-body" id="ticket">
+                        <div class="text-center">
+                            <b>Regular Recover</b><br>
+                            <h5>00/-</h5>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xl-3">
+                <div class="card flat-card">
+                    <div class="row-table">
+                        <div class="col-md-4 col-sm-2 card-body">
+                             <img src="{{asset('Backend/images/icon')}}/coin.png" alt="">     
+                        </div>
+                        <div class="col-sm-8 card-body" id="ticket">
+                        <div class="text-center">
+                            <b>Dou Recover</b><br>
+                            <h5>00/-</h5>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xl-3">
+                <div class="card flat-card">
+                    <div class="row-table">
+                        <div class="col-md-4 col-sm-2 card-body">
+                             <img src="{{asset('Backend/images/icon')}}/money-sack.png" alt="">     
+                        </div>
+                        <div class="col-sm-8 card-body" id="ticket">
+                        <div class="text-center">
+                            <b>Advance Recover</b><br>
+                            <h5>00/-</h5>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xl-3">
+                <div class="card flat-card">
+                    <div class="row-table">
+                        <div class="col-md-4 col-sm-2 card-body">
+                             <img src="{{asset('Backend/images/icon')}}/receipt.png" alt="">     
+                        </div>
+                        <div class="col-sm-8 card-body" id="ticket">
+                        <div class="text-center">
+                            <b>Expire Recover</b><br>
+                            <h5>00/-</h5>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xl-3">
+                <div class="card flat-card">
+                    <div class="row-table">
+                        <div class="col-md-4 col-sm-2 card-body">
+                             <img src="{{asset('Backend/images/icon')}}/dollar-symbol.png" alt="">     
+                        </div>
+                        <div class="col-sm-8 card-body" id="ticket">
+                        <div class="text-center">
+                            <b>Todays Disbursment</b><br>
+                            <h5>00/-</h5>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xl-3">
+                <div class="card flat-card">
+                    <div class="row-table">
+                        <div class="col-md-4 col-sm-2 card-body">
+                             <img src="{{asset('Backend/images/icon')}}/trend.png" alt="">     
+                        </div>
+                        <div class="col-sm-8 card-body" id="ticket">
+                        <div class="text-center">
+                            <b>Todays Deposit Receive</b><br>
+                            <h5>00/-</h5>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xl-3">
+                <div class="card flat-card">
+                    <div class="row-table">
+                        <div class="col-md-4 col-sm-2 card-body">
+                             <img src="{{asset('Backend/images/icon')}}/trend.png" alt="">     
+                        </div>
+                        <div class="col-sm-8 card-body" id="ticket">
+                        <div class="text-center">
+                            <b>Todays Deposit Payment</b><br>
+                            <h5>00/-</h5>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xl-3">
+                <div class="card flat-card">
+                    <div class="row-table">
+                        <div class="col-md-4 col-sm-2 card-body">
+                             <img src="{{asset('Backend/images/icon')}}/trend.png" alt="">     
+                        </div>
+                        <div class="col-sm-8 card-body" id="ticket">
+                        <div class="text-center">
+                            <b>Todays Cash Receive</b><br>
+                            <h5>00/-</h5>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xl-3">
+                <div class="card flat-card">
+                    <div class="row-table">
+                        <div class="col-md-4 col-sm-2 card-body">
+                             <img src="{{asset('Backend/images/icon')}}/trend.png" alt="">     
+                        </div>
+                        <div class="col-sm-8 card-body" id="ticket">
+                        <div class="text-center">
+                            <b>Todays Cash Payment</b><br>
+                            <h5>00/-</h5>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xl-3">
+                <div class="card flat-card">
+                    <div class="row-table">
+                        <div class="col-md-4 col-sm-2 card-body">
+                             <img src="{{asset('Backend/images/icon')}}/trend.png" alt="">     
+                        </div>
+                        <div class="col-sm-8 card-body" id="ticket">
+                        <div class="text-center">
+                            <b>Todays Income</b><br>
+                            <h5>00/-</h5>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xl-3">
+                <div class="card flat-card">
+                    <div class="row-table">
+                        <div class="col-md-4 col-sm-2 card-body">
+                             <img src="{{asset('Backend/images/icon')}}/trend.png" alt="">     
+                        </div>
+                        <div class="col-sm-8 card-body" id="ticket">
+                        <div class="text-center">
+                            <b>Todays Expense</b><br>
+                            <h5>00/-</h5>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-xl-3">
+                <div class="card flat-card">
+                    <div class="row-table">
+                        <div class="col-md-4 col-sm-2 card-body">
+                             <img src="{{asset('Backend/images/icon')}}/trend.png" alt="">     
+                        </div>
+                        <div class="col-sm-8 card-body" id="ticket">
+                        <div class="text-center">
+                            <b>হাতে নগদ</b><br>
+                            <h5>00/-</h5>
+                        </div>
                         </div>
                     </div>
                 </div>
