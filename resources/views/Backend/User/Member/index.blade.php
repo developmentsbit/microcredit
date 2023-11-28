c609a22e2eb97a86bb0a509a8993366b6f2b9f56
tanim0299 <tanimchy417@gmail.com>
2023-11-28 17:15:14 +0600

    TAN28-11-2

--- resources/views/Backend/User/Member/index.blade.php
+++ resources/views/Backend/User/Member/index.blade.php
@@ -26,8 +26,12 @@
             <!-- table card-1 start -->
             <div class="col-sm-12">
                 <div class="card">
+                    <div class="col-12 mt-2">
+                        <input type="text" class="form-control form-control-sm" placeholder="Search By Name or Member ID" onchange="getMember()" id="searchData">
+                    </div>
                     <div class="card-header">
                         <div class="row">
+
                             <div class="col-6">
                                 <h5>গ্রাহকের তথ্য</h5>
                             </div>
@@ -48,13 +52,11 @@
                                     <th>ফোন</th>
                                     <th>এন আই ডি</th>
                                     <th>স্ট্যাটাস</th>
-                                    {{-- <th>বিস্তারিত</th> --}}
                                     <th>ছবি</th>
-                                    {{-- <th>সাইন</th> --}}
                                     <th>অ্যাকশান</th>
                                 </tr>
                             </thead>
-                            <tbody>
+                            <tbody id="showMember">
 
 
 
@@ -66,6 +68,42 @@
         <!-- Latest Customers end -->
     </div>
     <!-- [ Main Content ] end -->
+
+
+    <script>
+        function getMember()
+        {
+            let data = $('#searchData').val();
+            // alert(data);
+            if(!data)
+            {
+                alert('Give Some Information To Search');
+            }
+            else
+            {
+                $.ajax({
+                    headers : {
+                        'X-CSRF-TOKEN' : '{{ csrf_token() }}',
+                    },
+
+                    url : '{{ url('getMember') }}',
+
+                    type : 'POST',
+
+                    data : {data},
+
+                    beforeSend : function(d){
+                        $('#showMember').html('Loading........');
+                    },
+
+                    success : function(response)
+                    {
+                        $('#showMember').html(response);
+                    }
+                });
+            }
+        }
+    </script>
 
     <script type="text/javascript">
         $(function () {
